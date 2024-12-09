a3e91db95f37f0d3c83bce1e5788dbadd590f9fd
prajna131 <2200030861@kluniversity.in>
2024-12-09 16:26:46 +0530

    msg

--- src/main/java/com/klef/jfsd/exam/controller/BookController.java
+++ src/main/java/com/klef/jfsd/exam/controller/BookController.java
@@ -6,37 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("api")
 public class BookController {
+
     @Autowired
     private BookService bookService;
 
-    @PostMapping
-    public ResponseEntity<Book> createBook(@RequestBody Book book) {
-        return ResponseEntity.ok(bookService.createBook(book));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Book>> getAllBooks() {
-        return ResponseEntity.ok(bookService.getAllBooks());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
-        return ResponseEntity.ok(bookService.getBookById(id));
-    }
-
     @PutMapping("/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
-        return ResponseEntity.ok(bookService.updateBook(id, updatedBook));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteBook(@PathVariable Long id) {
-        bookService.deleteBook(id);
-        return ResponseEntity.ok("Book deleted successfully");
+    public ResponseEntity<Book> updateBook(@PathVariable("id") Long bookId, @RequestBody Book updatedBook) {
+        Book book = bookService.updateBook(bookId, updatedBook);
+        return ResponseEntity.ok(book);
     }
 }
--- src/main/java/com/klef/jfsd/exam/service/BookServiceImpl.java
+++ src/main/java/com/klef/jfsd/exam/service/BookServiceImpl.java
@@ -6,39 +6,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookServiceImpl implements BookService {
+
     @Autowired
     private BookRepository bookRepository;
 
     @Override
-    public Book createBook(Book book) {
-        return bookRepository.save(book);
+    public Book updateBook(Long bookId, Book updatedBook) {
+        Optional<Book> existingBookOptional = bookRepository.findById(bookId);
+        if (existingBookOptional.isPresent()) {
+            Book existingBook = existingBookOptional.get();
+            existingBook.setTitle(updatedBook.getTitle());
+            existingBook.setAuthor(updatedBook.getAuthor());
+            return bookRepository.save(existingBook);
+        }
+        throw new RuntimeException("Book with ID " + bookId + " not found");
     }
 
-    @Override
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
-    }
+	@Override
+	public Book createBook(Book book) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public Book getBookById(Long id) {
-        return bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
-    }
+	@Override
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public Book updateBook(Long id, Book updatedBook) {
-        Book existingBook = getBookById(id);
-        existingBook.setTitle(updatedBook.getTitle());
-        existingBook.setAuthor(updatedBook.getAuthor());
-        existingBook.setIsbn(updatedBook.getIsbn());
-        return bookRepository.save(existingBook);
-    }
+	@Override
+	public Book getBookById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public void deleteBook(Long id) {
-        Book existingBook = getBookById(id);
-        bookRepository.delete(existingBook);
-    }
+	@Override
+	public void deleteBook(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 }
